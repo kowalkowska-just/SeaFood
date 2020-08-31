f34ea43739a115a169d292f5a30863442579b2db
Justyna <kowalkowska.just@gmail.com>
2020-08-31 19:59:11 +0200

    Recognizing Hotdogs is ready

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -29,9 +29,45 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickerImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickerImage
+            
+            guard let ciImage = CIImage(image: userPickerImage) else {
+                fatalError("Could not convert to CIImage.")
+            }
+            
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
