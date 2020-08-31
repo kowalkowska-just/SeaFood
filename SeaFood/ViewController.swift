082048b841e4189377827a030702375b4c5f7cd1
Justyna <kowalkowska.just@gmail.com>
2020-08-31 16:39:34 +0200

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Justyna Kowalkowska on 31/08/2020.
+//  Copyright © 2020 Justyna Kowalkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
